e0ffda28292d8f4530bb71b70cd0be61ab16affd
Frank Caron <frank@frankcaron.com>
2019-01-31 10:41:14 -0500

    Initial Commit

---
+++ miner-mobileTests/miner_mobileTests.swift
@@ -0,0 +1,34 @@
+//
+//  miner_mobileTests.swift
+//  miner-mobileTests
+//
+//  Created by Frank Caron on 1/31/19.
+//  Copyright © 2019 Frank Caron. All rights reserved.
+//
+
+import XCTest
+@testable import miner_mobile
+
+class miner_mobileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
